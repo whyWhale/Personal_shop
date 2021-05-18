adadebf8313686f769134624e4b4dab37d3c3228
KBY-TECH <soccer_by@naver.com>
2021-05-19 05:32:32 +0900

    [feat_StockManagementSystem-2 [server]] :  ResponseDTO add.

--- src/main/java/jpa/jpa_shop/web/controller/dto/response/BookUpdateResponseDto.java
+++ src/main/java/jpa/jpa_shop/web/controller/dto/response/BookUpdateResponseDto.java
@@ -1,24 +1,22 @@
 package jpa.jpa_shop.web.controller.dto.response;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import jpa.jpa_shop.domain.item.Book;
+import lombok.*;
 
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 
-@NoArgsConstructor
-@Getter
+@Data
 public class BookUpdateResponseDto {
-    Long id;
+    private Long id;
+
     @NotEmpty(message = "이름을 입력해주세요.")
     private String name;
 
-    @Min(value = 1000,message = "1000원 이상이여야 합니다.")
+    @Min(value = 1000, message = "1000원 이상이여야 합니다.")
     private int price;
 
-    @Min(value = 1,message = "1개 이상이여야 합니다.")
+    @Min(value = 1, message = "1개 이상이여야 합니다.")
     private int stockQuantity;
 
     @NotEmpty(message = "저자를 입력해주세요.")
@@ -28,13 +26,26 @@ public class BookUpdateResponseDto {
     private String isbn;
 
     @Builder
-    public BookUpdateResponseDto(Long id,String name, int price, int stockQuantity, String author, String isbn) {
-        this.id=id;
+    public BookUpdateResponseDto(Long id, String name, int price, int stockQuantity, String author, String isbn) {
+        this.id = id;
         this.name = name;
         this.price = price;
         this.stockQuantity = stockQuantity;
         this.author = author;
         this.isbn = isbn;
+    }
+
+    public Book toEntity() {
+        Book book = Book.builder()
+                .name(name)
+                .price(price)
+                .stockQuantity(stockQuantity)
+                .author(author)
+                .isbn(isbn)
+                .build();
+
+        book.setId(id);
+        return book;
     }
 
 }
