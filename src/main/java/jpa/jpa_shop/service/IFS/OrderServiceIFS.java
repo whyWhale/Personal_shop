8853917e460c3dca96727acaa00fc40f207e3e10
KBY-TECH <soccer_by@naver.com>
2021-05-31 03:11:53 +0900

    [update] : orderCreate Api 작성, OrderServiceIFS (order Overriding), Order 객체 create Overriding) , OrderSaveRequestDto 생성.

--- src/main/java/jpa/jpa_shop/service/IFS/OrderServiceIFS.java
+++ src/main/java/jpa/jpa_shop/service/IFS/OrderServiceIFS.java
@@ -1,6 +1,7 @@
 package jpa.jpa_shop.service.IFS;
 
 import jpa.jpa_shop.domain.orders.Order;
+import jpa.jpa_shop.web.controller.dto.request.order.OrderSaveRequestDto;
 import jpa.jpa_shop.web.controller.dto.request.order.OrderSearchRequestDto;
 
 import java.util.List;
@@ -8,6 +9,8 @@ import java.util.List;
 public interface OrderServiceIFS {
 
     public Long order(Long memberId, Long itemId, int count);
+
+    public Long order(OrderSaveRequestDto orderSaveRequestDto);
 
     public void cancelOrder(Long orderId);
 
