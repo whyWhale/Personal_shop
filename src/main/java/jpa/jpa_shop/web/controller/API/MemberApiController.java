2680d054d6dc01fd0c7996a362711b0a2dc277fc
KBY-TECH <soccer_by@naver.com>
2021-07-04 04:14:32 +0900

    [fix] : MemberApiController return type 변경

--- src/main/java/jpa/jpa_shop/web/controller/API/MemberApiController.java
+++ src/main/java/jpa/jpa_shop/web/controller/API/MemberApiController.java
@@ -27,10 +27,10 @@ public class MemberApiController {
     }
 
     @PostMapping("")
-    public void save(@Valid @RequestBody MemberSaveRequestDto requestDto)
+    public Long save(@Valid @RequestBody MemberSaveRequestDto requestDto)
     {
         log.info("{}","create memberName -- >"+requestDto.getName());
-        memberService.Join(requestDto.toEntity());
+        return memberService.Join(requestDto.toEntity());
     }
 
     @PutMapping("/{id}")
