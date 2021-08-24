efe85cfe17bdd27ffd426af29043b1c5c218bbd8
KBY-TECH <soccer_by@naver.com>
2021-08-24 21:25:26 +0900

    chore : travis , codeDeploy reSettings && deployScript write

---
+++ scripts/deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+REPOSITORY=/home/ec2-user/app/step2
+PROJECT_NAME=StockManagementSystem
+
+echo "> build file copy"
+
+cp $REPOSITORY/zip/*.jar $REPOSITORY/
+
+echo "> 현재 구동 중인 애플리케이션 PID Check! "
+
+CURRENT_PID=$(pgrep -fl StockManagementSystem | grep jar | awk '{print $1}')
+
+echo "> 현재 구동 중인 애플리케이션 PID = : $CURRENT_PID "
+
+if [ -z "$CURRENT_PID"]; then
+  echo "> 현재 구동 중인 애플리케이션이 없습니다. 그러므로그래서 종료하지 않습니다. "
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> new application deployment !"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR_NAME = $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행 권한 추가"
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행 !"
+
+nohup java -jar \
+              -Dspring.config.location=classpath:/application.yaml,/home/ec2-user/app/step1/application-release.yaml \
+              $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+
+
