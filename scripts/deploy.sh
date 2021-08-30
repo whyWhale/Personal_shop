c96438613cd3ff0a2f5c8611000de5a215de82f6
KBY-TECH <soccer_by@naver.com>
2021-08-30 19:04:55 +0900

    chore :  [deploy shell updated] { previous process Don't kill }

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 PID Check! "
 
-CURRENT_PID=$(pgrep -fl springboot-whywhale2 | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot-whywhale2 | pgrep java | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 PID = : $CURRENT_PID "
 
