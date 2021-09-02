fbff499c3bcc0c20eef40df3d8c9d08ab67680ca
KBY-TECH <soccer_by@naver.com>
2021-09-02 15:04:12 +0900

    chore : deploy shell 들여쓰기 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -13,7 +13,7 @@ CURRENT_PID=$(pgrep -fl springboot-whywhale2 | pgrep java | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 PID = : $CURRENT_PID "
 
-if [ -z "$CURRENT_PID"]; then
+if [ -z "$CURRENT_PID" ]; then
   echo "> 현재 구동 중인 애플리케이션이 없습니다. 그러므로그래서 종료하지 않습니다. "
 else
   echo "> kill -15 $CURRENT_PID"
